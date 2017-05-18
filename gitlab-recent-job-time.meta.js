fcfa233061b958caaf89b10b30d2014173c4bc1d
Ryan Leonard <rhinoisnot@gmail.com>
2017-05-18 11:47:58 -0400

    Preparing for v0.1.0.

--- gitlab-recent-job-time.meta.js
+++ gitlab-recent-job-time.meta.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         GitLab Recent Job Time
 // @namespace    http://codelenny.com/
-// @version      0.00000000
+// @version      0.00010000
 // @description  Queries GitLab to determine how long a CI job took the last time it was run.
 // @author       Ryan Leonard
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
